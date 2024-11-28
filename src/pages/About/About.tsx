3b34cd66d4a40629c38f98ee1f482268df4556a2
Thomas Mills <mills.thoma@gmail.com>
2024-11-28 18:16:36 +0100

    feat(about.tsx): new about page created (#16)

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,10 @@
+import type { FunctionComponent } from 'react';
+
+const Home: FunctionComponent = () => (
+  <div>
+    <p>About</p>
+    <p>Welcome to about</p>
+  </div>
+);
+
+export default Home;
